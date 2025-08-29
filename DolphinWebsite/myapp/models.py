f6f897607a445e9a21bb947dafe2a32dd5700877
Unknown-Dolphin <162408564+Unknown-Dolphin@users.noreply.github.com>
2025-08-29 13:40:15 +0700

    database and static(all chapter 2)

--- DolphinWebsite/myapp/models.py
+++ DolphinWebsite/myapp/models.py
@@ -7,4 +7,8 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, \
                                 null=True, blank=True)
-    quantity = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    instock = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- DolphinWebsite/myapp/views.py
+++ DolphinWebsite/myapp/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def home(request):
-    return render(request, 'myapp/home.html')
+    allproduct = Product.objects.all()
+    context = {'pd': allproduct}
+    return render(request, 'myapp/home.html', context)
 
 def home2(request):
     return HttpResponse('<h1><a href="http://127.0.0.1:8000/">Hello world2</a></h1>')

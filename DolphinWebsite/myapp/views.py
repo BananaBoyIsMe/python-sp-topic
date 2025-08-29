6fce2f971ae7180256dd762137ab99c79ae604f6
Unknown-Dolphin <162408564+Unknown-Dolphin@users.noreply.github.com>
2025-08-29 11:34:12 +0700

    First commit

---
+++ DolphinWebsite/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def home2(request):
+    return HttpResponse('<h1><a href="http://127.0.0.1:8000/">Hello world2</a></h1>')
+

0663ae789130a0222c29f7feaed735c88e8663ad
Unknown-Dolphin <162408564+Unknown-Dolphin@users.noreply.github.com>
2025-09-12 15:06:30 +0700

    week 5

--- DolphinWebsite/myapp/urls.py
+++ DolphinWebsite/myapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('about/', aboutUs, name="about-page"),
     path('contact/', contact, name="contact-page"),
     path('showcontact/', showContact, name="showcontact-page"),
+    path('register/', userRegist, name="register-page"),
+    path('profile/', userProfile, name="profile-page"),
+    path('editprofile/', editProfile, name="editprofile-page"),
 ]
--- DolphinWebsite/myapp/views.py
+++ DolphinWebsite/myapp/views.py
@@ -6,6 +6,8 @@ from .models import *
 
 from django.contrib.auth import authenticate, login
 
+from django.contrib.auth.models import User
+
 # Create your views here.
 def home(request):
     allproduct = Product.objects.all()
@@ -65,3 +67,70 @@ def showContact(request):
     # allcontact = contactList.objects.all().order_by('-id') # reverse list
     context = {'contact': allcontact}
     return render(request, 'myapp/showcontact.html', context)
+
+def userRegist(request):
+    context = {}
+
+    if request.method == "POST":
+        data = request.POST.copy()
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        repassword = data.get('repassword')
+    
+        try:
+            User.objects.get(username = username)
+            context['message'] = "Username Duplicate"
+        except:
+            newuser = User()
+            newuser.username = username
+            newuser.first_name = firstname
+            newuser.last_name = lastname
+            newuser.email = email
+            
+            if (password == repassword):
+                newuser.set_password(password)
+                newuser.save()
+                newprofile = Profile()
+                newprofile.user = User.objects.get(username = username)
+                newprofile.save()
+                context['message'] = "register complete"
+            else:
+                context['message'] = "password or re-password is incorrect"
+    return render(request, 'myapp/register.html', context)
+
+def userProfile(request):
+    context = {}
+    userprofile = Profile.objects.get(user=request.user)
+    context['profile'] = userprofile
+    return render(request, 'myapp/profile.html', context)
+
+def editProfile(request):
+    context = {}
+    if request.method == "POST":
+        data = request.POST.copy()
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
+        current_user = User.objects.get(id=request.user.id)
+        current_user.first_name = firstname
+        current_user.last_name = lastname
+        current_user.username = username
+        current_user.email = email
+        current_user.set_password(password)
+        current_user.save()
+
+        try:
+            user = authenticate(username=current_user.username, 
+                                password=current_user.password)
+            login(request, user)
+            return redirect('home-page')
+        except:
+            context['message'] = "edit profile fail"
+
+    return render(request, 'myapp/editprofile.html')
